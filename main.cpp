230f595872274d298f641ac68a621985290e4b5d
ASverdlov <sverdlovalbert@gmail.com>
2017-06-28 21:51:38 +0300

    Add threads

--- main.cpp
+++ main.cpp
@@ -5,39 +5,65 @@
 
 #include <boost/filesystem.hpp>
 
+#include <cstdlib>
 #include <iostream>
 #include <string>
-
-#include <cstdlib>
+#include <thread>
+#include <vector>
 
 namespace fs = boost::filesystem;
 
 using std::cout;
 using std::endl;
+using std::ref;
 using std::string;
 using std::to_string;
+using std::thread;
+using std::vector;
 
-int main(int argc, char* argv[]) {
-  Config config;
-  config.ParseOptions(argc, argv);
+static void CheckFileExists(const string& filename) {
+  if (!fs::exists(filename)) {
+    cout << "Error!\n" << filename << " doesn't exist.\n";
+    exit(EXIT_FAILURE);
+  }
+}
 
-  Trie actions_aggregator;
+static string Filename(const string& basename, size_t id) {
+  return basename + "/file" + to_string(id) + ".log";
+}
 
-  for (size_t i = 1; i <= config.FilesNum; ++i) {
-    const auto filename = config.Directory + "/file" + to_string(i) + ".log";
-
-    if (!fs::exists(filename)) {
-      cout << "ERROR! " << filename << " doesn't exist.\n";
-      return EXIT_FAILURE;
-    }
-
+static void ParseSliceOfFiles(size_t start_id, const Config& config,
+                             Trie& actions_aggregator) {
+  for (size_t id = start_id; id <= config.FilesNum; id += config.JobsNum) {
+    const auto filename = Filename(config.Directory, id);
     LogParser parser;
     parser.Parse(filename, [&](const Action& action_parsed) {
         actions_aggregator.Add(action_parsed.Properties);
     });
   }
+}
 
-  // Output aggregated actions
+int main(int argc, char* argv[]) {
+  /* Prepare */
+  Config config;
+  config.ParseOptions(argc, argv);
+
+  if (config.JobsNum == -1)
+    config.JobsNum = std::thread::hardware_concurrency();
+
+  for (size_t i = 1; i <= config.FilesNum; ++i)
+    CheckFileExists(Filename(config.Directory, i));
+
+  /* Aggregate */
+  Trie actions_aggregator;
+
+  vector<thread> workers(config.JobsNum);
+  for (size_t id = 1; id <= config.JobsNum; ++id)
+    workers[id] = thread(ParseSliceOfFiles, id, ref(config), ref(actions_aggregator));
+  for (size_t id = 1; id <= config.JobsNum; ++id)
+    workers[id].join();
+
+  /* Output */
   auto Outputer = [](const Trie::Key& key, Trie::Value value) {
     cout << "Record ";
     for (int x : key)
